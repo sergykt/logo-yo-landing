bc76d082d75e9a9a7a812706fc38585caf03d67b
sergykt <sergykt1992@mail.ru>
2023-10-27 23:19:37 +0600

    little fixes, header height var

--- src/js/components/popup.js
+++ src/js/components/popup.js
@@ -1,16 +1,24 @@
 const offerButtons = document.querySelectorAll('.offer__button');
+const headerButton = document.querySelector('.header__button');
 const popupEl = document.querySelector('.popup');
 const popupForm = document.querySelector('#popup-form');
 const closeButton = document.querySelector('.form__close-button');
+const inputEl = document.querySelector('#popup-name');
 
 const onClick = () => {
   if (popupEl.classList.contains('popup_active')) {
     document.body.style.overflow = 'auto';
   } else {
     document.body.style.overflow = 'hidden';
+    inputEl.focus();
   }
   popupEl.classList.toggle('popup_active');
 };
+
+headerButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  onClick();
+});
 
 offerButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
